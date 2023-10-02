2f3566011c47a3ad5f70183851a1f06f5e2f3985
MohamedTariqq <mohamed.tarek262@gmail.com>
2023-10-02 15:30:54 +0300

    Created The main project

---
+++ main.js
@@ -0,0 +1,9 @@
+let mainImg = document.querySelector(".img-main");
+let img = document.querySelector(".imga");
+let container = document.querySelector(".container");
+function phones(phone) {
+  mainImg.src = phone;
+}
+function colors(color) {
+  container.style.background = color;
+}
